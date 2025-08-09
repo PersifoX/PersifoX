bfcf042f95640919aef7c966a2c2b5ad04b8aaac
Alternative Vladislav <92142541+PersifoX@users.noreply.github.com>
2025-08-10 00:22:00 +0300

    Create docker-install-ubuntu.sh

---
+++ docker-install-ubuntu.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# A script to install Docker on Ubuntu.
+
+# Update the apt package index and install packages to allow apt to use a repository over HTTPS.
+sudo apt-get update
+sudo apt-get install ca-certificates curl gnupg
+
+# Add Docker's official GPG key.
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+# Set up the repository.
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# Update the apt package index again.
+sudo apt-get update
+
+# Install Docker Engine, containerd, and Docker Compose.
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# Verify that the Docker Engine installation is successful by running the hello-world image.
+sudo docker run hello-world
